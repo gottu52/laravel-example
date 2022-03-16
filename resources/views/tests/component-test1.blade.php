6c8ca432c2f836ab40ac7d2b8548b2e34222c2a3
gottu52 <ryohei846@ezweb.jp>
2022-03-16 10:21:55 +0900

    .

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     コンポーネント１
     <x-tests.card title="タイトル1" content="本文" :message="$message"/>
     <x-tests.card title="タイトル2" />
+    <x-tests.card title="cssの変更" class="bg-red-300"/>
 </x-tests.app>
