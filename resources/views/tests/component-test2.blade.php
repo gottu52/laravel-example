e88af83dca9ccdeeb743da14f1922b5576dfc5c0
gottu52 <ryohei846@ezweb.jp>
2022-03-16 11:26:40 +0900

    .

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,5 +1,9 @@
 <x-tests.app>
     <x-slot name="header">header2</x-slot>
    コンポーネント２
+   <div class="mb-4"></div>
+   <x-test-class-base classBaseMessage="メッセージ"/>
+   <div class="mb-4"></div>
+   <x-test-class-base classBaseMessage="メッセージ２" defaultMessage="変更済み" />
 </x-tests.app>
 
