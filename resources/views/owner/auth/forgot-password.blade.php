c8e1d336e2a447471ad1b4c1e55c21f6bedebc6d
gottu52 <ryohei846@ezweb.jp>
2022-03-17 17:08:24 +0900

    .

--- resources/views/owner/auth/forgot-password.blade.php
+++ resources/views/owner/auth/forgot-password.blade.php
@@ -16,7 +16,7 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('user.password.email') }}">
+        <form method="POST" action="{{ route('owner.password.email') }}">
             @csrf
 
             <!-- Email Address -->
